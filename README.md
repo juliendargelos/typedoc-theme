f37e348b4d3e59da7f20478cd6d5473424c6c5ec
Julien Dargelos <julien.dargelos@me.com>
2019-05-24 18:08:17 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# TypeDoc Theme
+
+TypeDoc theme extending [typedoc-default-themes](https://github.com/TypeStrong/typedoc-default-themes).
+
+[See the demo](https://juliendargelos.github.io/typedoc-theme)
---
+++ demo/index.ts
@@ -0,0 +1,67 @@
+export class Foo {
+  constructor(
+    parameter1: number,
+    parameter2: string,
+    parameter3: {
+      foo: string,
+      bar: boolean
+    }
+  ) {
+
+  }
+}
+
+export interface Bar1 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar2 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar3 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar4 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar5 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar6 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar7 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar8 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar9 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar10 {
+  blou: string
+  gna: Foo
+}
+
+export interface Bar11 {
+  blou: string
+  gna: Foo
+}
